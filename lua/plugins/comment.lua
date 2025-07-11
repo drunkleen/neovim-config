0befe2b90f25a22199c998ac6076973df3b8addd
DrunkLeen <snape@drunkleen.com>
2025-07-11 20:12:20 +0200

    multi-line comment added

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,12 +1,25 @@
--- lua/plugins/comment
+-- lua/plugins/comment.lua
 
 return {
 	"numToStr/Comment.nvim",
 	lazy = false,
 	config = function()
 		require("Comment").setup()
-		vim.keymap.set("n", "<leader>/", function()
-			require("Comment.api").toggle.linewise.current()
-		end, { desc = "Toggle comment for current line" })
+		local api = require("Comment.api")
+
+		-- Normal mode: toggle comment for current line
+		vim.keymap.set("n", "<leader>/", api.toggle.linewise.current, {
+			desc = "Toggle comment line",
+		})
+
+		-- Visual mode: toggle comment for selection
+		vim.keymap.set("v", "<leader>/", function()
+			local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+			vim.api.nvim_feedkeys(esc, "nx", false)
+
+			api.locked("toggle.linewise")(vim.fn.visualmode())
+		end, {
+			desc = "Toggle comment selection",
+		})
 	end,
 }
